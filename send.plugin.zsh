f0ef6e6ecd5fcf7e1dae94a0d19175bb0632348e
RobertZK <technoguyrob@gmail.com>
2015-10-04 11:39:24 -0500

    Use correct style guide.

--- send.plugin.zsh
+++ send.plugin.zsh
@@ -1,25 +1,25 @@
 pull() {
   if [ $# -eq 2 ]; then
-    git pull --rebase -q $2 `git rev-parse --abbrev-ref HEAD`
+    git pull --rebase -q $2 "$(git rev-parse --abbrev-ref HEAD)"
   else
-    git pull --rebase -q origin `git rev-parse --abbrev-ref HEAD`
+    git pull --rebase -q origin "$(git rev-parse --abbrev-ref HEAD)"
   fi
 }
 
 push() {
   if [ $# -eq 2 ]; then
-    git push -q $2 `git rev-parse --abbrev-ref HEAD`
+    git push -q $2 "$(git rev-parse --abbrev-ref HEAD)"
   else
-    git push -q origin `git rev-parse --abbrev-ref HEAD`
+    git push -q origin "$(git rev-parse --abbrev-ref HEAD)"
   fi
 }
 
 send() {
   git add "$(git rev-parse --show-toplevel)"
   if [ $# -eq 1 ]; then
-   git commit -a -m "$1"
+    git commit -a -m "$1"
   else
-   git commit -a -m "I'm too lazy to write a commit message."
+    git commit -a -m "I'm too lazy to write a commit message."
   fi
   pull
   push
