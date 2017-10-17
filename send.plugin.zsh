2a80c4e2f22f4bef0f22876ea40bc61dd0e8b9df
superhawk610 <superhawk610@gmail.com>
2017-10-17 13:23:58 -0400

    Added support for multiline commit messages
    send now accepts a second argument, which will be passed to `git commit` as the second line(s) of the commit message

--- send.plugin.zsh
+++ send.plugin.zsh
@@ -16,11 +16,11 @@ push() {
 
 send() {
   git add "$(git rev-parse --show-toplevel)"
-  if [ $# -eq 1 ]; then
-    git commit -a -m "$1"
-  else
-    git commit -a -m "I'm too lazy to write a commit message."
-  fi
+  case $# in
+    0) git commit -a -m "I'm too lazy to write a commit message.";;
+    1) git commit -a -m "$1";;
+    2) git commit -a -m "$1" -m "$2";;
+  esac
   pull
   push
 }
